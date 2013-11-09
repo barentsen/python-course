b0d5fe96ac765b7e037cdcdabe3c90e61fa25537
Geert Barentsen <geert@barentsen.be>
2013-11-09 00:44:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Python Programming Course
+=========================
+
+About
+-----
+
+These are the lecture notes used by Geert Barentsen for teaching a short 
+Python Programming course for 2nd-year Math/Physics/Astronomy undergraduates
+at the University of Hertfordshire. 
+
+License
+--------
+
+These notes have been adapted from an existing course called 
+"Python: Programming for Scientists" by Thomas Robitaille, 
+which is available here:
+   
+   https://github.com/astrofrog/py4sci
+
+Like the original work, these notes are licensed under the
+Attribution Non-Commercial Share-Alike Creative Commons License. For more
+information about the license, see the following page:
+
+  http://creativecommons.org/licenses/by-nc-sa/3.0/
+
---
+++ setup.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import os
+import glob
+
+from setuptools import setup, Command
+
+
+class BuildNotes(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        import os
+        import shutil
+        import tempfile
+
+        from IPython.nbconvert.nbconvertapp import NbConvertApp
+
+        import sys
+        for arg in range(len(sys.argv[1:])):
+            sys.argv.pop(-1)
+
+        # First convert the lecture notes to slides - we have to do them
+        # individually in order to be able to specify a custom output prefix for
+        # each.
+
+        app = NbConvertApp()
+        app.initialize()
+        app.export_format = 'slides'
+        app.config.Exporter.template_file = 'config/slides.tpl'
+        for notebook in glob.glob('lectures/*.ipynb'):
+            app.notebooks = [notebook]
+            app.output_base = notebook.replace('.ipynb', '')
+            app.start()
+
+        # Now convert the lecture notes, problem sets, and practice problems to
+        # HTML notebooks.
+
+        app = NbConvertApp()
+        app.initialize()
+        app.export_format = 'html'
+        for notebook in glob.glob('lectures/*.ipynb'):
+            app.notebooks = [notebook]
+            app.output_base = notebook.replace('.ipynb', '')
+            app.start()
+
+
+class DeployNotes(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        SERVER = os.environ["PY4SCI_SERVER"]
+        USER = os.environ["PY4SCI_USER"]
+
+        import getpass
+        from ftplib import FTP
+        from astropy.utils.console import ProgressBar
+
+        ftp = FTP(SERVER)
+        ftp.login(user=USER, passwd=getpass.getpass())
+
+        ftp.cwd('/public_html/PY4SCI_WS_2013_14')
+
+        for slides in ProgressBar.iterate(glob.glob('lectures/*.html')
+                                          + ['lectures/custom.css']
+                                          + glob.glob('problems/data/*')
+                                          + glob.glob('problems/*.html')
+                                          + glob.glob('practice/data/*')
+                                          + glob.glob('practice/*.html')):
+            try:
+                remote_size = ftp.size(slides)
+            except:
+                remote_size = None
+            local_size = os.path.getsize(slides)
+            if local_size != remote_size:
+                ftp.storbinary('STOR ' + slides, open(slides, 'rb'))
+
+        ftp.storbinary('STOR index.html', open('index.html', 'rb'))
+
+        ftp.quit()
+
+
+
+setup(name='python-course', cmdclass={'build': BuildNotes, 'deploy':DeployNotes})
